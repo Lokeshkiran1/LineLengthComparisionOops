68ab13060546b66db06c9080691ded391349d402
LokeshKiran <lokeshkskiran@gmail.com>
2022-09-05 20:38:44 +0530

    [Lokesh]Added:getting co-ordinates of two lines

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineLengthComparisionOops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Line Comoarision Program!");
+            Console.WriteLine("Choose Option:\n1.UC1-Getting the co-ordinates of two lines");
+            int choice=int.Parse(Console.ReadLine());
+            switch(choice)
+            {
+                case 1:
+                    GetCartesion.getCartesian();
+                    break;
+                default:
+                    Console.WriteLine("wrong choice");
+                    break;
+            }
+        }
+    }
+}
